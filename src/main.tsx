b8879e9cd08306c2a6bc5a101cfc32801a37d227
dhruv <dhruv.arora.da.3@gmail.com>
2026-03-09 21:55:45 +0530

    init

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from './Home';
+import About from './About';
+
+createRoot(document.getElementById('root')!).render(
+    <BrowserRouter>
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/about" element={<About />} />
+		</Routes>
+    </BrowserRouter>
+)
